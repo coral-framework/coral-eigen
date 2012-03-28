0ebb8940d1d7f5a75eeef64ad9e2726af3b10e03
Thiago Bastos <tabastos@gmail.com>
2012-03-27 22:45:17 -0300

    Attempt to fix random test failures by refactoring some unsafe coding practices…

--- tests/Vec3Tests.lua
+++ tests/Vec3Tests.lua
@@ -18,11 +18,9 @@ end
 -- Vec3 Tests
 -----------------------------
 
-local v1, v2, v3
-
 function basicsTest()
-	v1 = eigen.Vec3( 1, 1, 1 )
-	v2 = eigen.Vec3()
+	local v1 = eigen.Vec3( 1, 1, 1 )
+	local v2 = eigen.Vec3()
 	v2.x = 1
 	v2.y = 1
 	v2.z = 1
@@ -30,27 +28,25 @@ function basicsTest()
 end
 
 function mtOperationsTest()
-	v1 = eigen.Vec3( 1, 1, 1 )
-	v2 = eigen.Vec3( 1, 1, 1 )
-	v3 = eigen.Vec3()
-	v3 = v1 + v2
-	ASSERT_VEC_EQ( v3, eigen.Vec3( 2, 2, 2 ), "aaaa" )
+	local v1 = eigen.Vec3( 1, 1, 1 )
+	local v2 = eigen.Vec3( 1, 1, 1 )
+	local v3 = v1 + v2
+	ASSERT_VEC_EQ( v3, eigen.Vec3( 2, 2, 2 ) )
 	v1 = v2 + v3
-	ASSERT_VEC_EQ( v1, eigen.Vec3( 3, 3, 3 ), "ssss" )
+	ASSERT_VEC_EQ( v1, eigen.Vec3( 3, 3, 3 ) )
 
 	v1 = eigen.Vec3()
 	v2 = eigen.Vec3( 1, 1, 1 )
-	v3 = eigen.Vec3()
 	v3 = v2 * 2
-	ASSERT_VEC_EQ( v3, eigen.Vec3( 2, 2, 2 ), "bbbb" )
+	ASSERT_VEC_EQ( v3, eigen.Vec3( 2, 2, 2 ) )
 	v1 =  v2 * 2
-	ASSERT_VEC_EQ( v1, v3, "aaa" )
+	ASSERT_VEC_EQ( v1, v3 )
 end
 
 function basicOpsTest()
-	v1 = eigen.Vec3( 1, 1, 1 )
-	v2 = eigen.Vec3( 1, 1, 1 )
-	v3 = eigen.Vec3()
+	local v1 = eigen.Vec3( 1, 1, 1 )
+	local v2 = eigen.Vec3( 1, 1, 1 )
+	local v3 = eigen.Vec3()
 	eigen.addVec( v1, v2, v3 )
 	ASSERT_VEC_EQ( v3, eigen.Vec3( 2, 2, 2 ) )
 	v1 = eigen.addVec( v2, v3 )
@@ -74,15 +70,15 @@ function basicOpsTest()
 end
 
 function crossTest()
-	v1 = eigen.Vec3( 0, 1, 0 )
-	v2 = eigen.Vec3( 0, 0, 1 )
+	local v1 = eigen.Vec3( 0, 1, 0 )
+	local v2 = eigen.Vec3( 0, 0, 1 )
 	local v3 = eigen.crossVec( v1, v2 )
 	ASSERT_VEC_EQ( v3, eigen.Vec3( 1, 0, 0 ) )
 end
 
 function dotTest()
-	v1 = eigen.Vec3( 0, 1, 0 )
-	v2 = eigen.Vec3( 0, 0, 1 )
+	local v1 = eigen.Vec3( 0, 1, 0 )
+	local v2 = eigen.Vec3( 0, 0, 1 )
 	local result = eigen.dotVec( v1, v2 )
 	ASSERT_DOUBLE_EQ( result, 0 )
 	v1.z = 1
@@ -92,17 +88,18 @@ function dotTest()
 end
 
 function normLengthTest()
-	v1 = eigen.Vec3( 0, 3, 4 )
+	local v1 = eigen.Vec3( 0, 3, 4 )
+	local v2 = eigen.Vec3()
 	ASSERT_DOUBLE_EQ( eigen.length( v1 ), 5 )
 	eigen.normalize( v1, v2 )
 	ASSERT_VEC_EQ( v2, eigen.Vec3( 0, 3/5, 4/5 ) )
 end
 
 function QuatMulTest()
-	v1 = eigen.Vec3( 1, 0, 0 )
+	local v1 = eigen.Vec3( 1, 0, 0 )
 	local q = eigen.Quat()
 	eigen.rotateQuat( q, 90, eigen.Vec3( 0, 1, 0 ), q )
-	v2 = eigen.Vec3( 0, 0, -1 )
+	local v2 = eigen.Vec3( 0, 0, -1 )
 	v1 = q * v1
 	ASSERT_VEC_EQ( v1, v2 )
 	v1 = eigen.Vec3( 1, 0, 0 )
