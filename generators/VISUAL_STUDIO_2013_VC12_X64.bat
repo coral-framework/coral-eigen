be3262a0cf7cb2ae5520509ef43bcd09df487335
Rodrigo Braga Pinheiro <rbragapinheiro@gmail.com>
2015-06-19 17:07:18 -0300

    Removendo MANUAL_SETTINGS

--- generators/VISUAL_STUDIO_2013_VC12_X64.bat
+++ generators/VISUAL_STUDIO_2013_VC12_X64.bat
@@ -10,7 +10,7 @@ chdir vc12_x64
 
 del CMakeCache.txt
 
-cmake -G "Visual Studio 12 2013 Win64" -DMANUAL_TESTING=0 -DCMAKE_INSTALL_PREFIX="%CORAL_ROOT%" ../../
+cmake -G "Visual Studio 12 2013 Win64" -DCMAKE_INSTALL_PREFIX="%CORAL_ROOT%" ../../
 
 if %errorlevel% NEQ 0 goto error
 goto end
